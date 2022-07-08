46d3df367369fc5b47dca23181923304783a92fb
Yves de Sá <yvesdsb98@gmail.com>
2022-07-08 12:14:28 -0300

    feat: inital project config

---
+++ app/src/main/java/me/dio/soccer_news/ui/favorites/FavoritesFragment.java
@@ -0,0 +1,37 @@
+package me.dio.soccer_news.ui.favorites;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import me.dio.soccer_news.databinding.FragmentFavoritesBinding;
+
+public class FavoritesFragment extends Fragment {
+
+    private FragmentFavoritesBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        FavoritesViewModel dashboardViewModel =
+                new ViewModelProvider(this).get(FavoritesViewModel.class);
+
+        binding = FragmentFavoritesBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textDashboard;
+        dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
